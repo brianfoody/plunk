65d74e2d9ae9fc309d1ceb65e3cb6df36c2017f9
Avi Santoso <avi.santoso@gmail.com>
2025-11-14 17:36:18 +0800

    chore: Add --platform flag to docker build
    - Explicitly set the Docker build platform to linux/amd64 in the docker-push.sh script.

--- docker-push.sh
+++ docker-push.sh
@@ -4,6 +4,6 @@ DOCKER_USERNAME=${DOCKER_USERNAME:-"avisantoso"}
 IMAGE_NAME=${IMAGE_NAME:-"plunk"}
 VERSION=${VERSION:-"latest"}
 
-docker build -t ${DOCKER_USERNAME}/${IMAGE_NAME}:${VERSION} .
+docker build --platform linux/amd64 -t ${DOCKER_USERNAME}/${IMAGE_NAME}:${VERSION} .
 
 docker push ${DOCKER_USERNAME}/${IMAGE_NAME}:${VERSION}
